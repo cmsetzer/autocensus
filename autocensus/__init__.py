602988f211499ba6eecedd4af1c3dec26a6bc252
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2019-08-28 11:39:23 -0700

    Update version

--- autocensus/__init__.py
+++ autocensus/__init__.py
@@ -1,6 +1,6 @@
 """Package for collecting ACS and geospatial data from the Census API."""
 
-__version__ = '1.0.1'
+__version__ = '1.0.2'
 
 from . import api, errors, geography, query, socrata, topics, utilities  # noqa
 from .query import Query  # noqa
