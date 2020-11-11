2e10edbb32aefac51659b676a19ad3e66a11a613
Christopher Setzer <37311998+cmsetzer@users.noreply.github.com>
2020-11-10 20:57:56 -0800

    Use importlib.metadata for __version__

--- autocensus/__init__.py
+++ autocensus/__init__.py
@@ -1,13 +1,19 @@
 """Package for collecting ACS and geospatial data from the Census API."""
 
-__version__ = '1.1.0'
-
 import logging
 from logging import Logger, StreamHandler
 
 from . import api, errors, geography, query, socrata, utilities  # noqa
 from .query import Query  # noqa
 from .utilities import clear_cache  # noqa
+
+try:
+    from importlib.metadata import version
+except ImportError:
+    from importlib_metadata import version  # type: ignore
+
+# Initialize version string from package metadata
+__version__ = version(__name__)
 
 # Initialize logger
 logger: Logger = logging.getLogger(__name__)
